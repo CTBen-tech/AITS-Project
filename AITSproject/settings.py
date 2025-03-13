4f557eea095effd8c3c3f939f71a1ca7b563e634
Bakaluba Benjamin <bakalubabenjamin@gmail.com>
2025-03-13 21:52:57 +0300

    at this step the superuser has been set and is able to view the adminstartion page

--- AITSproject/settings.py
+++ AITSproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aits',#this is my app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- aits/models.py
+++ aits/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser #this line imports the abstract user function in django
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    Roles = (
+        ('admin','Admin'),
+        ('lecturer','Lecturer'),
+        ('registra','Registra'),
+        ('student','Student'),
+    )
+    role = models.CharField(max_length=10,choices=Roles, default = 'student')
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_groups", #Added related_name
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="customuser_permissions", #Added related_name
+        related_query_name="user",
+    )
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    Priority_Choices=(
+        ('low','Low'),
+        ('meduim','Meduim'),
+        ('high','High'),
+    )
+    Status_Choices=(
+        ('open','Open'),
+        ('in_progress','In Progress'),
+        ('resolved','Resolved'),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    department = models.ForeignKey(Department, on_delete = models.CASCADE)
+    priority = models.CharField(max_length=10, choices=Priority_Choices, default='low')
+    status = models.CharField(max_length=20,choices=Status_Choices,default='open')
+    created_by = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
