02e366911d06d1ff2191bce6d6d899f2d31855e8
Bakaluba Benjamin <bakalubabenjamin@gmail.com>
2025-04-11 00:03:57 +0300

    we have added the frontend url in the CORS_ALLOWED_ORIGINS to enable django to interact with react

--- backend/AITSproject/settings.py
+++ backend/AITSproject/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
 #added for the cors
 
 CORS_ALLOWED_ORIGINS = [
+    "https://aits-project.onrender.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
