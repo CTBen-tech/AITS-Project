51a88ededcedecbf3072a6284c16c687f29a661a
CTBen-tech <bakalubabenjamin@gmail.com>
2025-04-26 00:11:35 +0300

    Update settings.py commeted other origin in settings to see whether registration is possible on net

--- backend/AITSproject/settings.py
+++ backend/AITSproject/settings.py
@@ -60,10 +60,10 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://aits-project.onrender.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    #"http://localhost:3000",
+    #"http://127.0.0.1:3000",
+    #"http://localhost:8000",
+    #"http://127.0.0.1:8000",
 ]
 CORS_ALLOW_METHODS = [
     "GET",
