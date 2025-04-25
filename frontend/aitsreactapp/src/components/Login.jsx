02c36f7bebb5fd8c29fb973f4f57401acfda9c46
CTBen-tech <bakalubabenjamin@gmail.com>
2025-04-25 08:58:52 +0300

    Update Login.jsx added ..

--- frontend/aitsreactapp/src/components/Login.jsx
+++ frontend/aitsreactapp/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./styles/login.css";
+import "../styles/login.css";
 import { Link, useNavigate } from "react-router-dom";
 import {
   FaUser,
