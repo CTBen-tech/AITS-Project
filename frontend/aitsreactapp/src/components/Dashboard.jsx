121d08a2f0f57067bf9d1cf6ad3cf3a6dd4666d1
CTBen-tech <bakalubabenjamin@gmail.com>
2025-04-25 06:45:57 +0300

    Update Dashboard.jsx removed a fullstop cause it was preventing deployement

--- frontend/aitsreactapp/src/components/Dashboard.jsx
+++ frontend/aitsreactapp/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '.dashboard.css';
+import 'dashboard.css';
 
 function Dashboard() {
   const [stats, setStats] = useState({ open: 0, in_progress: 0, resolved: 0 });
