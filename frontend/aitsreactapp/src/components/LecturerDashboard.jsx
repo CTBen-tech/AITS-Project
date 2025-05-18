8df18372118e051c2cee35cdff5f89f3340191a2
Benjamin <bakalubabenjamin@gmail.com>
2025-05-18 18:50:10 +0300

    created lecturer and registra dashboard

---
+++ frontend/aitsreactapp/src/components/LecturerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LecturerDashboard() {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+      <p>Welcome, Student!</p>
+    </div>
+  );
+}
+
+export default LecturerDashboard;
