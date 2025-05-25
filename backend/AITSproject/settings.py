5ed3e6fff0fc675541de98504b58d157467666a8
Benjamin <bakalubabenjamin@gmail.com>
2025-05-25 19:28:38 +0300

    preparing for redeploying by updating .env, settings.py

--- backend/AITSproject/settings.py
+++ backend/AITSproject/settings.py
@@ -63,10 +63,9 @@ MIDDLEWARE = [
 #added for the cors
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "https://aits-project.onrender.com",
-    "http://127.0.0.1:8000",
-    
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",    
 
 ]
 
